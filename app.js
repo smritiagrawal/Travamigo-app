044ca5126b91a3b6d80b8b489d3bc34303c24c87
Smriti Agrawal <smritiagrawal.iitkgp@gmail.com>
2018-02-04 12:20:41 -0800

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+angular.module("Travamigo",['main']);
+
+angular.module("main",[]);
+
+ angular.module("main").controller("mainController",function($scope){
+    // Controller body
+    var data = "Roger";
+    $scope.data = "Smriti is awesome!!!";
+    $scope.temp = "Chaitanya is guy"
+});
